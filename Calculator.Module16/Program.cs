e929ef0d6fe835513d11a4fa76a56a4b54ad4f4e
MRX19814 <128835508+MRX19814@users.noreply.github.com>
2023-07-30 19:39:15 +0300

    Add files via upload

---
+++ Calculator.Module16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practices
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
